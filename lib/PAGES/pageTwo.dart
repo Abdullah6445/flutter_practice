4b6b705951bba1363445d604e9549a3e0461d62b
abdullah <abdullahlaptop6445@gmail.com>
2024-03-07 13:40:09 +0500

    adding listview.seperated

--- lib/PAGES/pageTwo.dart
+++ lib/PAGES/pageTwo.dart
@@ -8,24 +8,61 @@ class pageTwo extends StatefulWidget {
 }
 
 class _pageTwoState extends State<pageTwo> {
+  List<String> names = [
+    "abdullah",
+    "zeeshan",
+    "amir",
+    "ammar",
+    "hasnat",
+    "abdullah",
+    "zeeshan",
+    "amir",
+    "ammar",
+    "hasnat"
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("page 2 flutter practice"),
+      appBar: AppBar(
+        title: Text("page 2 flutter practice"),
+      ),
+      body: Column(children: [
+        Container(
+          height: MediaQuery.of(context).size.height * (.3),
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  child: Image.asset("assets/images/img.png"),
+                ),
+                title: Text(
+                  names[index],
+                  style: TextStyle(fontWeight: FontWeight.w900),
+                ),
+                subtitle: Text(
+                  "03317012500",
+                  style: TextStyle(fontWeight: FontWeight.w900),
+                ),
+                trailing: Icon(Icons.call),
+              );
+            },
+            itemCount: names.length,
+          ),
         ),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            return ListTile(
-              leading: CircleAvatar(
-                child: Image.asset("assets/images/img.png"),
-              ),
-              title: Text("abdullah"),
-              subtitle: Text("03317012500"),
-              trailing: Icon(Icons.call),
-            );
-          },
-          itemCount: 50,
-        ));
+        Container(
+          height: MediaQuery.of(context).size.height * (.3),
+          child: ListView.separated(
+              itemBuilder: (context, index) {
+                return Text(names[index]);
+              },
+              separatorBuilder: (context, index) {
+                return Divider(
+                  thickness: 3,
+                );
+              },
+              itemCount: names.length),
+        ),
+      ]),
+    );
   }
 }
